73aa851c9fa285c5018c6f27217dde875b789af9
mark <vec@icareum.com>
2023-09-07 05:46:56 +0800

    seo module

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .PySeoHtml import PySeoHtml
---
+++ publish_to_pypi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# chmod +x publish_to_pypi.sh
+# ./publish_to_pypi.sh
+
+# Step 1: Build the distribution packages
+python setup.py sdist bdist_wheel
+
+# Step 2: Upload the distribution packages to PyPI using twine
+twine upload dist/*
+
+# Step 3: Clean up the temporary build files (optional)
+rm -rf build dist *.egg-info
